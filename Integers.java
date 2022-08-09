46e77716d4f59b03c8ac59f438103b9931f72048
NandakumarTS <https://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@github.com/NareshBabuO/Core-java.git>
2022-08-09 17:38:56 +0530

    first prgm

---
+++ Integers.java
@@ -0,0 +1,20 @@
+class Integers
+{
+	public static void main (String args[])
+	{
+		int a=Integer.max(50,20);
+		System.out.println("the Maximum value of integer is "+a);
+		 int kitkat=Integer.bitCount(20);
+		 System.out.println(kitkat);
+		 int sum=Integer.sum(40,25);
+		 System.out.println(sum);
+		 int p=Integer.parseInt("3");
+		 System.out.println(p);
+		 int b=Integer.min(42,75);
+		 System.out.println(b);
+         int d=Integer.decode("3");
+         System.out.println(d);
+        int c=Integer.compare(71,52);
+      System.out.println(c);		
+	}
+}
