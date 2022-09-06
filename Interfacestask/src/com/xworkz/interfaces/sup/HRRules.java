a5f57dfdabf3348fb33b8a1d1d3c4ed6ac98f85a
NareshBabuO <nareshbabu.xworkz@gmail.com>
2022-09-06 10:40:42 +0530

    Interfacs Rules

--- Interfacestask/src/com/xworkz/interfaces/sup/HRRules.java
+++ Interfacestask/src/com/xworkz/interfaces/sup/HRRules.java
@@ -1,9 +1,29 @@
 package com.xworkz.interfaces.sup;
 
-public interface HRRules {
+public interface HRRules extends ManagerRules,SecurityRules{
 
 	boolean applyLeaves();
 
 	boolean harrasment();
+	
+@Override
+default boolean swipeCard() {
+	// TODO Auto-generated method stub
+	return false;
+}
+@Override
+default boolean parkingApproval() {
+	// TODO Auto-generated method stub
+	return false;
+}
+@Override
+default boolean informLeave() {
+	// TODO Auto-generated method stub
+	return false;
+}@Override
+default boolean wfh() {
+	// TODO Auto-generated method stub
+	return false;
+}
 
 }
--- Interfacestask/src/com/xworkz/interfaces/sup/ITRuleFollowers.java
+++ Interfacestask/src/com/xworkz/interfaces/sup/ITRuleFollowers.java
@@ -9,26 +9,31 @@ public class ITRuleFollowers implements HRRules, SecurityRules, ManagerRules {
 
 	}
 
+	@Override
 	public boolean harrasment() {
 		System.out.println("if any complaint comes,you need to resign ");
 		return true;
 	}
 
+	@Override
 	public boolean informLeave() {
 		System.out.println("Need aproval from Manager for Leave");
 		return true;
 	}
 
+	@Override
 	public boolean wfh() {
 		System.out.println("ask manager for wfh");
 		return true;
 	}
 
+	@Override
 	public boolean swipeCard() {
 		System.out.println("if want to enter office u need to swipeCard");
 		return true;
 	}
 
+	@Override
 	public boolean parkingApproval() {
 		System.out.println("need parkingApproval form security team ");
 		return false;
