076bb0dae966125600da1eaddccafa1600d53652
NareshBabuO <nareshbabu.xworkz@gmail.com>
2022-09-15 23:34:20 +0530

    Service And Exceptions

--- DAO&Services/src/com/xworkz/bridge/dao/service/PrimeMinisterService.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.bridge.dao.service;
-
-import com.xworkz.bridge.exception.InvalidPlaceException;
-
-public interface PrimeMinisterService {
-	
-	boolean ValiddateAndSave(String name)throws InvalidPlaceException;
-
-}
